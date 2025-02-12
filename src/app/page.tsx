3e82de4da840697793fc8442ce610224605121e9
Flames <isaacomenuche@gmail.com>
2025-02-13 00:22:00 +0100

    feat: add mobile responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,10 +16,10 @@ export default function Home() {
 	}, []);
 
 	return (
-		<div className="border border-border rounded-[40px] bg-card p-12 space-y-8 max-w-[700px] mx-auto">
+		<div className="border border-border rounded-[32px] sm:rounded-[40px] bg-card p-6 sm:p-12 space-y-8 max-w-[700px] mx-auto">
 			<div className="space-y-3">
-				<div className="flex items-center justify-between">
-					<h1 className="text-[32px] font-normal">
+				<div className="flex flex-col sm:flex-row sm:items-center justify-between gap-3">
+					<h1 className="text-2xl sm:text-[32px] font-normal">
 						Ticket Selection
 					</h1>
 					<p className="font-roboto text-base font-normal">
@@ -29,18 +29,20 @@ export default function Home() {
 				<Progress value={progress} className="" />
 			</div>
 
-			<div className="space-y-8 p-6">
-				<div className="banner backdrop-blur-[7px] border-[2px] border-input rounded-3xl p-6 text-center space-y-2">
-					<h2 className="font-road text-[62px] font-normal leading-[100%]">
-						Techember Fest ”25
-					</h2>
-					<p className="font-roboto text-base font-normal leading-[150%] max-w-[340px] mx-auto">
-						Join us for an unforgettable experience at [Event Name]!
-						Secure your spot now.
-					</p>
-					<p className="font-roboto text-base font-normal leading-[150%] space-x-4">
+			<div className="space-y-8 sm:p-6">
+				<div className="banner backdrop-blur-[7px] border-[2px] border-input rounded-3xl py-4 px-6 sm:p-6 text-center sm:space-y-2 flex flex-col justify-between h-[243px] sm:h-full ">
+					<div className="space-y-2">
+						<h2 className="font-road text-5xl sm:text-[62px] font-normal leading-[100%]">
+							Techember Fest ”25
+						</h2>
+						<p className="font-roboto text-sm sm:text-base font-normal leading-[150%] sm:max-w-[340px] mx-auto">
+							Join us for an unforgettable experience at [Event
+							Name]! Secure your spot now.
+						</p>
+					</div>
+					<p className="font-roboto text-base font-normal leading-[150%] flex flex-col sm:flex-row justify-center gap-1 sm:gap-4">
 						<span>📍 [Event Location]</span>
-						<span>| |</span>
+						<span className="hidden sm:inline-flex">| |</span>
 						<span>March 15, 2025 | 7:00 PM</span>
 					</p>
 				</div>
@@ -50,7 +52,7 @@ export default function Home() {
 					setSelectedSeat={setSelectedSeat}
 				/>
 				<TicketNum ticketNum={ticketNum} setTicketNum={setTicketNum} />
-				<div className="w-full flex gap-6">
+				<div className="w-full flex flex-col sm:flex-row gap-4 sm:gap-6">
 					<Button
 						variant={"outline"}
 						className="w-full text-primary leading-[150%]"
--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -14,7 +14,7 @@ const TabsList = React.forwardRef<
 	<TabsPrimitive.List
 		ref={ref}
 		className={cn(
-			"flex justify-between rounded-3xl bg-[#052228] border border-input p-4",
+			"flex flex-col sm:flex-row justify-between gap-6 sm:gap-0 rounded-3xl bg-[#052228] border border-input p-4",
 			className
 		)}
 		{...props}
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
 	<TabsPrimitive.Trigger
 		ref={ref}
 		className={cn(
-			"whitespace-nowrap text-start min-w-[158px] rounded-xl p-3 ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+			"whitespace-nowrap text-start sm:min-w-[158px] rounded-xl p-3 ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
 			"border-2 border-[#197686] bg-transparent", // Default (Inactive)
 			"data-[state=active]:bg-[#12464E] data-[state=active]:border-[1px] data-[state=active]:border-[#197686] data-[state=active]:text-white",
 			className
