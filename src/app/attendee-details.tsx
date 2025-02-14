903786c61119d067673eaac817c9949a24aad188
Flames <isaacomenuche@gmail.com>
2025-02-14 02:10:34 +0100

    fix: move sensitive data to .env config

--- src/app/attendee-details.tsx
+++ src/app/attendee-details.tsx
@@ -73,11 +73,14 @@ export default function AttendeeDetails({
 
 		const formData = new FormData();
 		formData.append("file", file);
-		formData.append("upload_preset", "chill guy");
+		formData.append(
+			"upload_preset",
+			process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET || ""
+		);
 
 		try {
 			const response = await fetch(
-				"https://api.cloudinary.com/v1_1/dapbvli1v/image/upload",
+				`https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
 				{
 					method: "POST",
 					body: formData,
